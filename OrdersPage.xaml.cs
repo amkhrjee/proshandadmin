e195f2171dbd840c1a1bde978e2535cdac1aff4e
Aniruddha Mukherjee <aniruddha.mukherji@outlook.com>
2024-05-23 09:41:23 +0530

    add: model generation

--- OrdersPage.xaml.cs
+++ OrdersPage.xaml.cs
@@ -16,6 +16,11 @@ using System.Threading;
 using Microsoft.UI.Xaml.Media.Animation;
 using Windows.System;
 using System.Windows.Input;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Newtonsoft.Json;
+using System.IO;
+using Google.Cloud.Firestore;
 
 
 namespace proshandadmin
@@ -151,6 +156,98 @@ namespace proshandadmin
 
             NewOrdersCount.Text = null;
             NewOrdersCount.Text = newOrdersCount.ToString();
+        }
+
+        // Process new orders
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ProcessButtonText.Text = "Processing";
+                ProcessButton.IsEnabled = false;
+                var settingsFile = await StorageFile.GetFileFromPathAsync(ApplicationData.Current.LocalFolder.Path + "\\settings.json");
+
+                string json = File.ReadAllText(settingsFile.Path);
+                var settings = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                var thirtyPath = settings["Thirty"].Replace('>', '\\');
+                var thirtyFivePath = settings["ThirtyFive"].Replace('>', '\\');
+                var fortyPath = settings["Forty"].Replace('>', '\\');
+                var fortyFivePath = settings["FortyFive"].Replace('>', '\\');
+                var outputDirPath = settings["OutputDir"].Replace('>', '\\');
+                var outputFolder = await StorageFolder.GetFolderFromPathAsync(outputDirPath);
+
+
+                FirestoreDb db = FirestoreDb.Create("prosthetichand-c0f57");
+
+                foreach (var order in orders)
+                {
+
+                    if (!order.IsDone)
+                    {
+                        var orderId = order.Id;
+                        var handSize = order.HandSize;
+                        var handColor = order.HandColor;
+                        var presetPath = "";
+
+                        // copy the appropriate size from the preset folder
+                        switch (handSize)
+                        {
+                            case "30":
+                                presetPath = thirtyPath;
+                                break;
+                            case "35":
+                                presetPath = thirtyFivePath;
+                                break;
+                            case "40":
+                                presetPath = fortyPath;
+                                break;
+                            case "45":
+                                presetPath = fortyFivePath;
+                                break;
+                        }
+
+                        var presetModel = await StorageFile.GetFileFromPathAsync(presetPath);
+                        await presetModel.CopyAsync(outputFolder, $"{orderId}.gltf", NameCollisionOption.ReplaceExisting);
+
+                        // Write to Firestore with updated isDone value
+                        DocumentReference docRef = db.Collection("Orders").Document(orderId);
+                        await docRef.UpdateAsync(new Dictionary<string, object>
+                        {
+                            {"isDone", true }
+                        });
+                    }
+                }
+                ContentDialog dialog = new()
+                {
+                    XamlRoot = this.XamlRoot,
+                    Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
+                    Title = "Models Generated",
+                    PrimaryButtonText = "View Models",
+                    CloseButtonText = "OK",
+                    Content = "Press the Refresh button to view updated data",
+                    PrimaryButtonCommand = new RelayCommand(async() => await Launcher.LaunchFolderAsync(outputFolder))
+                };
+                await dialog.ShowAsync();
+                ProcessButton.IsEnabled = true;
+                ProcessButtonText.Text = "Proess New Orders";
+            }
+            catch (Exception ex)
+            {
+                ContentDialog dialog = new()
+                {
+                    XamlRoot = this.XamlRoot,
+                    Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
+                    Title = "Error occured while generating models",
+                    PrimaryButtonText = "Network Settings",
+                    CloseButtonText = "Close App",
+                    DefaultButton = ContentDialogButton.Primary,
+                    Content = $"Some error occured while generating models.\n\nError Description: {ex.Message}",
+                    PrimaryButtonCommand = new RelayCommand(OpenNetworkSettings),
+                    CloseButtonCommand = new RelayCommand(() => Application.Current.Exit()),
+                };
+                dialog.Closing += (s, e) => Application.Current.Exit();
+                await dialog.ShowAsync();
+            }
         }
     }
 }
